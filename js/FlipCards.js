5808f4dbf9a974176e71ef0c31a3c4f1473e75c6
Tonija <tonija.mijatovic@gmail.com>
2017-03-27 18:11:15 +0200

    ...

---
+++ js/FlipCards.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$(document).ready(function(){
+
+  $(".card").flip({
+  axis: 'y',
+  trigger: 'hover'
+  });
+    
+});
+
+
+
---
+++ js/PlayAudio.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var snd = new Audio ("meow.wav");
+
+$(document).ready(function(){
+	var snd = new Audio ("meow.wav");
+		$(".fa-github").click(function(){      
+	        snd.play();        
+	    }); 
+    
+});
+
+
+
